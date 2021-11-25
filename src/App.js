d44d2d114fba8127a049ffe256e7c89e76d270ef
ANHKHOA1905 <nguyenanhkhoa19051999@gmail.com>
2021-11-25 23:16:30 +0700

    config basic for project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "swiper/swiper.min.css";
+import "./assets/boxicons-2.0.9/css/boxicons.min.css";
+import "./App.scss";
+
+import Header from "./components/header/Header.js";
+import Footer from "./components/footer/Footer.js";
+
+import AppRoute from "./config/AppRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <AppRoute />
+      <Footer />
     </div>
   );
 }
